0e715ade9af3dd49467e079fb71d4e0b0d85aa8b
Arron M <marshall.arron@gmail.com>
2019-04-10 12:08:02 -0700

    Add close hitbox when menu opens

--- Article/Article.js
+++ Article/Article.js
@@ -1,3 +1,7 @@
+/**
+ * Class representing an Article.
+ * @name Article
+ */
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -18,6 +22,7 @@ class Article {
     // Set up height values for each article
     // DEBUG: Hard coded 22, because I'm too lazy to math out the padding/margin to get scalable values. Future me will be annoyed but at least I left a debug comment
     this.height = this.getArticleHeight.call(this) - 22;
+    console.log(this.height);
   }
 
   getArticleHeight() {
@@ -53,3 +58,53 @@ const articles = document.querySelectorAll('.article');
 
 // - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 articles.forEach((article) => new Article(article));
+
+
+// Create an ArticleFactory
+/**
+ * Creates a new Article object.
+ * @name ArticleFactory
+ * @property {object} props - An object containing data required to create an Article.
+ * @property {string} props.parent - CSS selector for the parent element.
+ * @property {string} props.title - Title for the Article.
+ * @property {string} props.date - Date the article was published.
+ * @property {string} props.content - Rich content for the article.
+ * @returns {Article}
+ */
+const ArticleFactory = (props) => {
+  // create dom structure .article>h2{title}+p.date{date}+content+span.expandButton+span.closeButton
+  const article = document.createElement('div');
+  article.classList.add('article');
+
+  // create title node
+  const title = document.createElement('h2');
+  title.appendChild(document.createTextNode(props.title));
+  
+  // create date node
+  const published = document.createElement('p');
+  published.classList.add('date');
+  published.appendChild(document.createTextNode(props.date));
+
+  // create content node
+  const content = document.createElement('div');
+  content.classList.add('article-content')
+  content.innerHTML = DOMPurify.sanitize(props.content);
+
+  // create expand button
+  const expand = document.createElement('span');
+  expand.classList.add('expandButton');
+
+  // create close button
+  const close = document.createElement('span');
+  close.classList.add('closeButton');
+  close.appendChild(document.createTextNode('close'));
+
+  // populate our article with child nodes
+  article.appendChild(title);
+  article.appendChild(published);
+  article.appendChild(content);
+  article.appendChild(expand);
+  article.appendChild(close);
+  document.querySelector(props.parent).appendChild(article);
+  new Article(article);
+}
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,6 @@
 const toggleMenu = () => {
   menu.style.display = 'block';
-  menu.classList.toggle('menu--open')
+  menu.classList.toggle('menu--open');
   if (menu.classList.contains('menu-open')) {
     setTimeout(() => {
       menu.style.display = 'none'
@@ -15,3 +15,7 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+// close box
+const closeBox = document.querySelector('.closeBox');
+closeBox.addEventListener('click', toggleMenu);
