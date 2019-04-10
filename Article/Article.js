54139c8739c99a2e734cde7d2df6fd0c7bc55dfa
Arron M <marshall.arron@gmail.com>
2019-04-10 10:23:57 -0700

    Add animation to article toggle

--- Article/Article.js
+++ Article/Article.js
@@ -2,7 +2,7 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-
+    
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
 
@@ -10,13 +10,32 @@ class Article {
     this.expandButton.innerText = 'expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.expandButton.addEventListener('click', this.toggleArticle.bind(this));
+
+    // Set up height values for each article
+    // DEBUG: Hard coded 22, because I'm too lazy to math out the padding/margin to get scalable values. Future me will be annoyed but at least I left a debug comment
+    this.height = this.getArticleHeight.call(this) - 22;
   }
 
-  // TODO: Nomenclature, probably 'toggleArticle'
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+  getArticleHeight() {
     this.domElement.classList.toggle('article-open');
+    const elementHeight = this.domElement.offsetHeight;
+    this.domElement.classList.toggle('article-open');
+    return elementHeight;
+  }
+
+  toggleArticle() {
+    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    const state = this.expandButton.textContent;
+    // NOTE: Bad nomenclature, 'collapsed' state === 'expand', may want to update this functionality for readability
+    if (state === 'expand') {
+      console.log(this.domElement);
+      this.expandButton.textContent = 'collapse';
+      this.domElement.style.height = `${this.height}px`;
+    } else {
+      this.expandButton.textContent = 'expand';
+      this.domElement.removeAttribute('style');
+    }
   }
 }
 
